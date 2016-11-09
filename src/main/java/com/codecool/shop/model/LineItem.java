f0206345a58ea117666643a36344c421d1ad9a54
Codecool-Palko <matolaypal@gmail.com>
2016-11-09 16:05:55 +0100

    delete LineItem main method (it was necessary just for test)

--- src/main/java/com/codecool/shop/model/LineItem.java
+++ src/main/java/com/codecool/shop/model/LineItem.java
@@ -36,22 +36,4 @@ public class LineItem {
     private void setSubTotalPrice() {
         this.subTotalPrice = this.quantity*this.product.getDefaultPrice();
     }
-
-
-
-
-    public static void main(String[] args) {
-        Supplier amazon = new Supplier("Amazon", "Digital content and services");
-        ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
-        Product product = new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon);
-
-        LineItem item = new LineItem(product);
-//        System.out.println(item.getProductName());
-//        System.out.println(item.getQuantity());
-//        System.out.println(item.getSubTotalPrice());
-        item.setQuantity(100);
-//        System.out.println(item.getQuantity());
-        System.out.println(item.getSubTotalPrice());
-    }
-
 }
