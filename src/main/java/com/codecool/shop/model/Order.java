b32fa23a0b0a2ef8063a649fc2e958c1434acccc
Codecool-Palko <matolaypal@gmail.com>
2016-11-10 13:33:56 +0100

    DATA list not static anymore, test printing deleted

--- src/main/java/com/codecool/shop/model/Order.java
+++ src/main/java/com/codecool/shop/model/Order.java
@@ -3,21 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
-    private static List<LineItem> DATA = new ArrayList<>();
-    private static Order instance = null;
-
-    /* A private Constructor prevents any other class from instantiating.
-     */
-    private Order() {
-    }
-
-    public static Order getInstance() {
-        if (instance == null) {
-            instance = new Order();
-        }
-        return instance;
-    }
-
+    private  List<LineItem> DATA = new ArrayList<>();
 
     public void addItem(LineItem newItem) {
         for (LineItem lineItem:DATA){
@@ -37,7 +23,6 @@ public class Order {
     public Integer getAllQuantity () {
         Integer res = 0;
         for (LineItem lineItem:DATA){
-            System.out.println("lineItem quantity: "+lineItem.getQuantity());
             res+=lineItem.getQuantity();
         }
         return res;
@@ -46,7 +31,6 @@ public class Order {
     public float getAllPrice() {
         float res = 0;
         for (LineItem lineItem:DATA){
-            System.out.println("lineItem Subprice: "+lineItem.getSubTotalPrice());
             res+=lineItem.getSubTotalPrice();
         }
         return res;
