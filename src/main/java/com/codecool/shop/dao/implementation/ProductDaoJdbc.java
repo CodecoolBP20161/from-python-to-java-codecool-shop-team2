5e82f4187fc614b9bd486ca07617e83dbed39ba7
adambdnr <adambodnar2@gmail.com>
2016-11-22 14:12:59 +0100

    productDaoJdbc ready

--- src/main/java/com/codecool/shop/dao/implementation/ProductDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoJdbc.java
@@ -1,20 +1,31 @@
 package com.codecool.shop.dao.implementation;
 
 import com.codecool.shop.controller.DatabaseController;
-import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
-import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class ProductDaoJdbc implements ProductDao {
 
     DatabaseController databaseController = new DatabaseController();
+
+    private static ProductDaoJdbc instance = null;
+
+    private ProductDaoJdbc() {
+    }
+
+    public static ProductDaoJdbc getInstance() {
+        if (instance == null) {
+            instance = new ProductDaoJdbc();
+        }
+        return instance;
+    }
 
     @Override
     public void add(Product product) {
@@ -36,7 +47,7 @@ public class ProductDaoJdbc implements ProductDao {
             stmt.setString(5, String.valueOf(product.getDefaultCurrency()));
             stmt.setString(6, String.valueOf(product.getProductCategory().getId()));
             stmt.setString(7, String.valueOf(product.getSupplier().getId()));
-            stmt.executeQuery();
+            stmt.executeUpdate();
         }catch (SQLException e) {
             e.printStackTrace();
         }
@@ -46,8 +57,28 @@ public class ProductDaoJdbc implements ProductDao {
     public Product find(int id) {
 
         String query = "SELECT * FROM product WHERE id ='" + id + "';";
-        databaseController.executeQuery(query);
+
+        try (Connection connection = databaseController.getConnection();
+             Statement statement =connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query);
+        ){
+            while (resultSet.next()){
+                Product product = new Product(
+                        resultSet.getString("name"),
+                        resultSet.getFloat("defaultprice"),
+                        resultSet.getString("currencystring"),
+                        resultSet.getString("description"),
+                        resultSet.getObject("productcategory", ProductCategory.class),
+                        resultSet.getObject("supplier", Supplier.class));
+                product.setId(resultSet.getInt(1));
+                return product;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         return null;
+
     }
 
 
@@ -61,22 +92,82 @@ public class ProductDaoJdbc implements ProductDao {
     @Override
     public List<Product> getAll() {
         String query = "SELECT * FROM product;";
-        databaseController.executeQuery(query);
-        return null;
+        List<Product> resultList = new ArrayList<>();
+
+        try (Connection connection = databaseController.getConnection();
+             Statement statement =connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query);
+        ){
+            while (resultSet.next()){
+                Product product = new Product(
+                        resultSet.getString("name"),
+                        resultSet.getFloat("defaultprice"),
+                        resultSet.getString("currencystring"),
+                        resultSet.getString("description"),
+                        resultSet.getObject("productcategory", ProductCategory.class),
+                        resultSet.getObject("supplier", Supplier.class));
+                product.setId(resultSet.getInt(1));
+                resultList.add(product);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return resultList;
     }
 
     @Override
     public List<Product> getBy(Supplier supplier) {
         String query = "SELECT * FROM product WHERE supplier ='" + supplier.getId() + "';";
-        databaseController.executeQuery(query);
-        return null;
+        List<Product> resultList = new ArrayList<>();
+
+        try (Connection connection = databaseController.getConnection();
+             Statement statement =connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query);
+        ){
+            while (resultSet.next()){
+                Product product = new Product(
+                        resultSet.getString("name"),
+                        resultSet.getFloat("defaultprice"),
+                        resultSet.getString("currencystring"),
+                        resultSet.getString("description"),
+                        resultSet.getObject("productcategory", ProductCategory.class),
+                        resultSet.getObject("supplier", Supplier.class));
+                product.setId(resultSet.getInt(1));
+                resultList.add(product);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return resultList;
     }
 
     @Override
     public List<Product> getBy(ProductCategory productCategory) {
         String query = "SELECT * FROM product WHERE category ='" + productCategory.getId() + "';";
-        databaseController.executeQuery(query);
-        return null;
+        List<Product> resultList = new ArrayList<>();
+
+        try (Connection connection = databaseController.getConnection();
+             Statement statement =connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query);
+        ){
+            while (resultSet.next()){
+                Product product = new Product(
+                        resultSet.getString("name"),
+                        resultSet.getFloat("defaultprice"),
+                        resultSet.getString("currencystring"),
+                        resultSet.getString("description"),
+                        resultSet.getObject("productcategory", ProductCategory.class),
+                        resultSet.getObject("supplier", Supplier.class));
+                product.setId(resultSet.getInt(1));
+                resultList.add(product);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return resultList;
     }
 
 }
--- src/main/java/com/codecool/shop/dao/implementation/SupplierDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/SupplierDaoJdbc.java
@@ -5,6 +5,7 @@ import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.model.Supplier;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,17 +14,28 @@ import java.util.List;
 public class SupplierDaoJdbc implements SupplierDao {
     DatabaseController databaseController = new DatabaseController();
 
+    private static SupplierDaoJdbc instance = null;
+
+    private SupplierDaoJdbc() {
+    }
+
+    public static SupplierDaoJdbc getInstance() {
+        if (instance == null) {
+            instance = new SupplierDaoJdbc();
+        }
+        return instance;
+    }
+
 
     @Override
     public void add(Supplier supplier) {
         try {
 
             PreparedStatement stmt;
-            stmt = databaseController.getConnection().prepareStatement("INSERT INTO codecoolshop (id, name, description) VALUES (?, ?, ?)");
-            stmt.setString(1, String.valueOf(supplier.getId()));
-            stmt.setString(2, supplier.getName());
-            stmt.setString(3, supplier.getDescription());
-            stmt.executeQuery();
+            stmt = databaseController.getConnection().prepareStatement("INSERT INTO supplier (name, description) VALUES (?, ?)");
+            stmt.setString(1, supplier.getName());
+            stmt.setString(2, supplier.getDescription());
+            stmt.executeUpdate();
         }catch (SQLException e) {
             e.printStackTrace();
         }
@@ -34,28 +46,57 @@ public class SupplierDaoJdbc implements SupplierDao {
     @Override
     public Supplier find(int id) {
 
-        String query = "SELECT * FROM codecoolshop WHERE id ='" + id + "';";
-
-        try {
-            Connection connection = databaseController.getConnection();
-            Statement statement =connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(query);
+        String query = "SELECT * FROM supplier WHERE id ='" + id + "';";
 
 
+        try (Connection connection = databaseController.getConnection();
+             Statement statement =connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query);
+        ){
+            while (resultSet.next()){
+                Supplier supplier = new Supplier(
+                        resultSet.getString("name"),
+                        resultSet.getString("description"));
+                supplier.setId(resultSet.getInt(1));
+                return supplier;
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
         return null;
+
     }
 
     @Override
     public void remove(int id) {
 
+        String query = "DELETE FROM supplier WHERE id ='" + id + "';";
+        databaseController.executeQuery(query);
     }
 
     @Override
     public List<Supplier> getAll() {
-        return null;
+
+        String query = "SELECT * FROM supplier;";
+
+        List<Supplier> resultList = new ArrayList<>();
+
+        try (Connection connection = databaseController.getConnection();
+             Statement statement =connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query);
+        ){
+            while (resultSet.next()){
+                Supplier supplier = new Supplier(resultSet.getString("name"),
+                        resultSet.getString("description"));
+                supplier.setId(resultSet.getInt(1));
+                resultList.add(supplier);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return resultList;
     }
+
 }
