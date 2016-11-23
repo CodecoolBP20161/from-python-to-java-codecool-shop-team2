af6effa682404704ab6dfd1b6bae7dc382863049
Codecool-Palko <matolaypal@gmail.com>
2016-11-23 12:10:34 +0100

    better name, better understanding

---
+++ src/main/java/com/codecool/shop/service/DatabaseService.java
@@ -0,0 +1,28 @@
+package com.codecool.shop.service;
+
+import java.sql.*;
+
+public class DatabaseService {
+
+    private static final String DATABASE = "jdbc:postgresql://localhost:5432/codecoolshop";
+    private static final String DB_USER = "postgres";
+    private static final String DB_PASSWORD = "postgres";
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(
+                DATABASE,
+                DB_USER,
+                DB_PASSWORD);
+    }
+
+    public void executeQuery(String query) {
+        try (Connection connection = getConnection();
+             Statement statement =connection.createStatement();
+        ){
+            statement.execute(query);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
