49bd8e12810f9bbc063427903024913307efa8b1
adambdnr <adambodnar2@gmail.com>
2016-11-24 14:57:09 +0100

    supplierDaoJdbc covered with tests

--- src/test/java/com/codecool/shop/dao/implementation/SupplierDaoJdbcTest.java
+++ src/test/java/com/codecool/shop/dao/implementation/SupplierDaoJdbcTest.java
@@ -3,56 +3,58 @@ package com.codecool.shop.dao.implementation;
 import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.model.Supplier;
 import com.codecool.shop.service.TestDatabaseService;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
+
 import java.sql.Statement;
 
 import static org.junit.Assert.*;
 
 public class SupplierDaoJdbcTest {
 
-    TestDatabaseService databaseService = new TestDatabaseService();
     SupplierDao testSupplierStore = SupplierDaoJdbc.getInstance();
-    Supplier apple = new Supplier("mac", "mac");
+    Supplier testSupplier = new Supplier("testSupplier", "supplier for testing");
 
     @Before
     public void setUp() {
-        testSupplierStore.remove(1);
-        apple.setId(1);
+        testSupplierStore.add(testSupplier);
     }
 
 
     @Test
     public void getInstance() throws Exception {
-        Supplier apple = new Supplier("Mac", "Digital content and services");
+
         assertTrue(testSupplierStore instanceof SupplierDaoJdbc);
 
     }
 
     @Test
     public void add() throws Exception {
-        testSupplierStore.add(apple);
-        assertEquals(apple, testSupplierStore.find(1));
+        testSupplier.setId(2);
+        assertEquals(testSupplier, testSupplierStore.find(2));
     }
+
 
     @Test
     public void find() throws Exception {
-        testSupplierStore.add(apple);
-        assertEquals(apple, testSupplierStore.find(1));
+        testSupplier.setId(3);
+        assertEquals(testSupplier, testSupplierStore.find(3));
     }
+
 
     @Test
     public void remove() throws Exception {
-        testSupplierStore.remove(1);
-        assertEquals(null, testSupplierStore.find(1));
+        int beforeSize = testSupplierStore.getAll().size();
+        testSupplierStore.remove(testSupplierStore.find(beforeSize).getId()-1);
+        int afterSize = testSupplierStore.getAll().size();
+        assertTrue(beforeSize - afterSize == 1);
     }
+
 
     @Test
     public void getAll() throws Exception {
-        testSupplierStore.add(apple);
-        assertEquals(1, testSupplierStore.getAll().size());
+        testSupplierStore.add(testSupplier);
+        int allSize = testSupplierStore.getAll().size();
+        assertEquals(testSupplierStore.getAll().size(), allSize);
 
     }
 
