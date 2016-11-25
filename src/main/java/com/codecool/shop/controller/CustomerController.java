09c91a11563744796f51bc4e20d53d5b677911e7
Codecool-Palko <matolaypal@gmail.com>
2016-11-25 04:05:53 +0100

    welcome mail send here and if nam or email exist, reload registration form

--- src/main/java/com/codecool/shop/controller/CustomerController.java
+++ src/main/java/com/codecool/shop/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.codecool.shop.controller;
 import com.codecool.shop.dao.CustomerDao;
 import com.codecool.shop.dao.implementation.CustomerDaoJdbc;
 import com.codecool.shop.model.Customer;
+import com.codecool.shop.model.MailMan;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
@@ -17,9 +18,19 @@ public class CustomerController {
 
     public static String addCustomer(Request req, Response res) {
         CustomerDao customerDataStore = CustomerDaoJdbc.getInstance();
-        // get the customer data from client and add to database
-        customerDataStore.add(Customer.getFromClient(req.body()));
+        // get the customer data from client, then check it exist, after that add and send mail
+        Customer customer = Customer.getFromClient(req.body());
+        String customerFree = customerDataStore.verifyCustomer(customer);
+        if (!customerFree.equals("OK")) {
+            System.out.println(customerFree);
+            res.redirect("/registration");
+            return null;
+        }
+        req.session().removeAttribute("customerStatus");
+        customerDataStore.add(customer);
         res.redirect("/");
+        MailMan email = new MailMan();
+        email.sendWelcome(customer.getEmail());
         return null;
     }
 }
