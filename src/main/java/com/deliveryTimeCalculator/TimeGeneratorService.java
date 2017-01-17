988fc618964a00a8c4974c5a4b1eeec1abc8ae2b
BTamara <bolyatami@gmail.com>
2017-01-17 10:49:35 +0100

    deliveryTime microservice merged

---
+++ src/main/java/com/deliveryTimeCalculator/TimeGeneratorService.java
@@ -0,0 +1,26 @@
+package com.deliveryTimeCalculator;
+/*
+Mapping all route for the API and link with the APIController.
+ */
+
+import com.deliveryTimeCalculator.service.APIService;
+import com.deliveryTimeCalculator.controller.APIController;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+
+public class TimeGeneratorService {
+    private APIController controller;
+
+    public static void main(String[] args) {
+        port(60003);
+
+        TimeGeneratorService application = new TimeGeneratorService();
+        application.controller = new APIController(APIService.getInstance());
+
+        // --- MAPPING ---
+        //for e.g.: http://0.0.0.0:60003/api/timecalculator/Budapest
+        get("/api/timecalculator/:destination", application.controller::location);
+    }
+
+}
